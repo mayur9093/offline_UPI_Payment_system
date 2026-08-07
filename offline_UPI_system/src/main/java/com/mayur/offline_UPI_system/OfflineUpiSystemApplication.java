439f80cbb8fe110ac9fd2e6d0927057f2c0941fe
mayur9093 <mayurpatilr9@gmail.com>
2026-08-08 00:46:00 +0530

    Initial commit

---
+++ offline_UPI_system/src/main/java/com/mayur/offline_UPI_system/OfflineUpiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mayur.offline_UPI_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineUpiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineUpiSystemApplication.class, args);
+	}
+
+}
---
+++ offline_UPI_system/src/test/java/com/mayur/offline_UPI_system/OfflineUpiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayur.offline_UPI_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineUpiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
